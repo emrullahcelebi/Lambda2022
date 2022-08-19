59c7d2e1f0b4d8b57c20aed3cc12e43397074bba
Emrullah Celebi <cumali20182019@gmail.com>
2022-08-19 11:39:30 +0200

    v2

---
+++ src/day03/Utils.java
@@ -0,0 +1,18 @@
+package day03;
+
+import java.util.List;
+
+public class Utils {
+
+    public static void bosluk(Object obj){
+        System.out.print(obj+" ");
+    }
+
+    public static boolean ciftYaz(int x){
+        return x%2==0;
+    }
+
+    public static boolean tekYaz(int y){
+        return y%2!=0;
+    }
+}
